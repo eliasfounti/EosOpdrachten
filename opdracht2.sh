8cb6bfa9c9bb257d00b871a819ba43e2406cb1dc
eliasfounti <113886742+eliasfounti@users.noreply.github.com>
2024-06-04 15:11:46 +0200

    opdracht 2 imagepick

--- opdracht2.sh
+++ opdracht2.sh
@@ -1,1 +1,12 @@
-#lukt niet ik doe via virtualbox. Downloaden van image werkt niet,
+#!/bin/bash
+
+MAP="$1"
+for BESTAND in "$MAP"/*.jpg; do
+    if [ -f "$BESTAND" ]; then
+        NAAM_ZONDER_EXTENSIE=$(basename "$BESTAND" .jpg)
+        convert "$BESTAND" -resize 128x128\> "$MAP/$NAAM_ZONDER_EXTENSIE.png"
+        echo "Geconverteerd: $BESTAND naar PNG"
+    fi
+done
+
+echo "Conversie voltooid."
